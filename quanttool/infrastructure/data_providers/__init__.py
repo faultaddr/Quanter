60e44a7f3267e16e35eba14d03199e354756dd4c
faultaddr <cuferpan@gmail.com>
2026-03-17 10:38:52 +0800

    fix: dataloader crash
    Signed-off-by: faultaddr <cuferpan@gmail.com>

--- quanttool/infrastructure/data_providers/__init__.py
+++ quanttool/infrastructure/data_providers/__init__.py
@@ -1,7 +1,7 @@
 """Data providers package for QuantTool."""
 
 # Import data providers to register them
-from . import tushare_provider, ashare_provider, csv_provider, real_data_provider, data_fetcher
+from . import tushare_provider, ashare_provider, csv_provider, real_data_provider, data_fetcher, incremental_data_provider
 from .async_data_fetcher import AsyncDataFetcher, fetch_symbols, fetch_symbols_async
 
 __all__ = [
@@ -10,6 +10,7 @@ __all__ = [
     'csv_provider',
     'real_data_provider',
     'data_fetcher',
+    'incremental_data_provider',
     'AsyncDataFetcher',
     'fetch_symbols',
     'fetch_symbols_async'
